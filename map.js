dd896733ce0a3af74c535ad56a76c57974370b7e
Charles <cweng@stuy.edu>
2018-04-12 00:03:52 -0400

    stuffs

---
+++ map.js
@@ -0,0 +1,37 @@
+/*
+  lefTwickz - Terry Guan, Caleb Smith-Salzberg, Charles Weng, Yuyang Zhang
+  SoftDev2   pd7
+  P #01: Viz
+  2018-4-?
+*/
+
+/*
+  ==============================================================================
+                                  Variables/Initiation
+  ==============================================================================
+*/
+
+var object, svgDoc, svgItem;
+
+window.onload=function() {
+	// Get the Object by ID
+	object = document.getElementById("svgObject");
+	// Get the SVG document inside the Object tag
+	// svgDoc = object.contentDocument;
+	// Get one of the SVG items by ID;
+	svgItem = document.getElementsByTagName("svg")[0];
+
+	svgItem.setAttribute("fill", "lime");
+};
+
+// add events for each path
+var paths = d3.select('svg').selectAll('path')
+paths.on("mouseover", function(){
+    this.style("fill", "red");
+  })
+paths.on("mouseout", function(){
+
+  })
+paths.on("click", function(){
+
+  });
