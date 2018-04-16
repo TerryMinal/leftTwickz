6353e4108d6c163575c37550bc7628f8194bf4a3
TerryMinal <terryguan513@gmail.com>
2018-04-16 19:11:40 -0400

    restructuring repo

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
+from util import api
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("map.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
